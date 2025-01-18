4eb281b44070b8f42a302db6bd8f987af35218e5
Jotaoesee <jadiazm1995@gmail.com>
2025-01-18 14:33:56 +0100

    termino los tiles para mi primer mapa

---
+++ assets/tiles/tilesets/paleta.tsx
@@ -0,0 +1,388 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1" name="paleta" tilewidth="48" tileheight="48" tilecount="128" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../../images/images_tiles/Layer-0.png" width="48" height="48"/>
+ </tile>
+ <tile id="1">
+  <image source="../../images/images_tiles/tile2.png" width="48" height="48"/>
+ </tile>
+ <tile id="2">
+  <image source="../../images/images_tiles/tile3.png" width="48" height="48"/>
+ </tile>
+ <tile id="3">
+  <image source="../../images/images_tiles/tile4.png" width="48" height="48"/>
+ </tile>
+ <tile id="4">
+  <image source="../../images/images_tiles/tile5.png" width="48" height="48"/>
+ </tile>
+ <tile id="5">
+  <image source="../../images/images_tiles/tile6.png" width="48" height="48"/>
+ </tile>
+ <tile id="6">
+  <image source="../../images/images_tiles/tile7.png" width="48" height="48"/>
+ </tile>
+ <tile id="7">
+  <image source="../../images/images_tiles/tile8.png" width="48" height="48"/>
+ </tile>
+ <tile id="8">
+  <image source="../../images/images_tiles/tile9.png" width="48" height="48"/>
+ </tile>
+ <tile id="9">
+  <image source="../../images/images_tiles/tile10.png" width="48" height="48"/>
+ </tile>
+ <tile id="10">
+  <image source="../../images/images_tiles/tile11.png" width="48" height="48"/>
+ </tile>
+ <tile id="11">
+  <image source="../../images/images_tiles/tile12.png" width="48" height="48"/>
+ </tile>
+ <tile id="12">
+  <image source="../../images/images_tiles/tile13.png" width="48" height="48"/>
+ </tile>
+ <tile id="13">
+  <image source="../../images/images_tiles/tile14.png" width="48" height="48"/>
+ </tile>
+ <tile id="14">
+  <image source="../../images/images_tiles/tile15.png" width="48" height="48"/>
+ </tile>
+ <tile id="15">
+  <image source="../../images/images_tiles/tile16.png" width="48" height="48"/>
+ </tile>
+ <tile id="16">
+  <image source="../../images/images_tiles/tile17.png" width="48" height="48"/>
+ </tile>
+ <tile id="17">
+  <image source="../../images/images_tiles/tile18.png" width="48" height="48"/>
+ </tile>
+ <tile id="18">
+  <image source="../../images/images_tiles/tile19.png" width="48" height="48"/>
+ </tile>
+ <tile id="19">
+  <image source="../../images/images_tiles/tile20.png" width="48" height="48"/>
+ </tile>
+ <tile id="20">
+  <image source="../../images/images_tiles/tile21.png" width="48" height="48"/>
+ </tile>
+ <tile id="21">
+  <image source="../../images/images_tiles/tile22.png" width="48" height="48"/>
+ </tile>
+ <tile id="22">
+  <image source="../../images/images_tiles/tile23.png" width="48" height="48"/>
+ </tile>
+ <tile id="23">
+  <image source="../../images/images_tiles/tile24.png" width="48" height="48"/>
+ </tile>
+ <tile id="24">
+  <image source="../../images/images_tiles/tile25.png" width="48" height="48"/>
+ </tile>
+ <tile id="25">
+  <image source="../../images/images_tiles/tile26.png" width="48" height="48"/>
+ </tile>
+ <tile id="26">
+  <image source="../../images/images_tiles/tile27.png" width="48" height="48"/>
+ </tile>
+ <tile id="27">
+  <image source="../../images/images_tiles/tile28.png" width="48" height="48"/>
+ </tile>
+ <tile id="28">
+  <image source="../../images/images_tiles/tile29.png" width="48" height="48"/>
+ </tile>
+ <tile id="29">
+  <image source="../../images/images_tiles/tile30.png" width="48" height="48"/>
+ </tile>
+ <tile id="30">
+  <image source="../../images/images_tiles/tile31.png" width="48" height="48"/>
+ </tile>
+ <tile id="31">
+  <image source="../../images/images_tiles/tile32.png" width="48" height="48"/>
+ </tile>
+ <tile id="32">
+  <image source="../../images/images_tiles/tile33.png" width="48" height="48"/>
+ </tile>
+ <tile id="33">
+  <image source="../../images/images_tiles/tile34.png" width="48" height="48"/>
+ </tile>
+ <tile id="34">
+  <image source="../../images/images_tiles/tile35.png" width="48" height="48"/>
+ </tile>
+ <tile id="35">
+  <image source="../../images/images_tiles/tile36.png" width="48" height="48"/>
+ </tile>
+ <tile id="36">
+  <image source="../../images/images_tiles/tile37.png" width="48" height="48"/>
+ </tile>
+ <tile id="37">
+  <image source="../../images/images_tiles/tile38.png" width="48" height="48"/>
+ </tile>
+ <tile id="38">
+  <image source="../../images/images_tiles/tile39.png" width="48" height="48"/>
+ </tile>
+ <tile id="39">
+  <image source="../../images/images_tiles/tile40.png" width="48" height="48"/>
+ </tile>
+ <tile id="40">
+  <image source="../../images/images_tiles/tile41.png" width="48" height="48"/>
+ </tile>
+ <tile id="41">
+  <image source="../../images/images_tiles/tile42.png" width="48" height="48"/>
+ </tile>
+ <tile id="42">
+  <image source="../../images/images_tiles/tile43.png" width="48" height="48"/>
+ </tile>
+ <tile id="43">
+  <image source="../../images/images_tiles/tile44.png" width="48" height="48"/>
+ </tile>
+ <tile id="44">
+  <image source="../../images/images_tiles/tile45.png" width="48" height="48"/>
+ </tile>
+ <tile id="45">
+  <image source="../../images/images_tiles/tile46.png" width="48" height="48"/>
+ </tile>
+ <tile id="46">
+  <image source="../../images/images_tiles/tile47.png" width="48" height="48"/>
+ </tile>
+ <tile id="47">
+  <image source="../../images/images_tiles/tile48.png" width="48" height="48"/>
+ </tile>
+ <tile id="48">
+  <image source="../../images/images_tiles/tile49.png" width="48" height="48"/>
+ </tile>
+ <tile id="49">
+  <image source="../../images/images_tiles/tile50.png" width="48" height="48"/>
+ </tile>
+ <tile id="50">
+  <image source="../../images/images_tiles/tile51.png" width="48" height="48"/>
+ </tile>
+ <tile id="51">
+  <image source="../../images/images_tiles/tile52.png" width="48" height="48"/>
+ </tile>
+ <tile id="52">
+  <image source="../../images/images_tiles/tile53.png" width="48" height="48"/>
+ </tile>
+ <tile id="53">
+  <image source="../../images/images_tiles/tile54.png" width="48" height="48"/>
+ </tile>
+ <tile id="54">
+  <image source="../../images/images_tiles/tile55.png" width="48" height="48"/>
+ </tile>
+ <tile id="55">
+  <image source="../../images/images_tiles/tile56.png" width="48" height="48"/>
+ </tile>
+ <tile id="56">
+  <image source="../../images/images_tiles/tile57.png" width="48" height="48"/>
+ </tile>
+ <tile id="57">
+  <image source="../../images/images_tiles/tile58.png" width="48" height="48"/>
+ </tile>
+ <tile id="58">
+  <image source="../../images/images_tiles/tile59.png" width="48" height="48"/>
+ </tile>
+ <tile id="59">
+  <image source="../../images/images_tiles/tile60.png" width="48" height="48"/>
+ </tile>
+ <tile id="60">
+  <image source="../../images/images_tiles/tile61.png" width="48" height="48"/>
+ </tile>
+ <tile id="61">
+  <image source="../../images/images_tiles/tile62.png" width="48" height="48"/>
+ </tile>
+ <tile id="62">
+  <image source="../../images/images_tiles/tile63.png" width="48" height="48"/>
+ </tile>
+ <tile id="63">
+  <image source="../../images/images_tiles/tile64.png" width="48" height="48"/>
+ </tile>
+ <tile id="64">
+  <image source="../../images/images_tiles/tile65.png" width="48" height="48"/>
+ </tile>
+ <tile id="65">
+  <image source="../../images/images_tiles/tile66.png" width="48" height="48"/>
+ </tile>
+ <tile id="66">
+  <image source="../../images/images_tiles/tile67.png" width="48" height="48"/>
+ </tile>
+ <tile id="67">
+  <image source="../../images/images_tiles/tile68.png" width="48" height="48"/>
+ </tile>
+ <tile id="68">
+  <image source="../../images/images_tiles/tile69.png" width="48" height="48"/>
+ </tile>
+ <tile id="69">
+  <image source="../../images/images_tiles/tile70.png" width="48" height="48"/>
+ </tile>
+ <tile id="70">
+  <image source="../../images/images_tiles/tile71.png" width="48" height="48"/>
+ </tile>
+ <tile id="71">
+  <image source="../../images/images_tiles/tile72.png" width="48" height="48"/>
+ </tile>
+ <tile id="72">
+  <image source="../../images/images_tiles/tile73.png" width="48" height="48"/>
+ </tile>
+ <tile id="73">
+  <image source="../../images/images_tiles/tile74.png" width="48" height="48"/>
+ </tile>
+ <tile id="74">
+  <image source="../../images/images_tiles/tile75.png" width="48" height="48"/>
+ </tile>
+ <tile id="75">
+  <image source="../../images/images_tiles/tile76.png" width="48" height="48"/>
+ </tile>
+ <tile id="76">
+  <image source="../../images/images_tiles/tile77.png" width="48" height="48"/>
+ </tile>
+ <tile id="77">
+  <image source="../../images/images_tiles/tile78.png" width="48" height="48"/>
+ </tile>
+ <tile id="78">
+  <image source="../../images/images_tiles/tile79.png" width="48" height="48"/>
+ </tile>
+ <tile id="79">
+  <image source="../../images/images_tiles/tile80.png" width="48" height="48"/>
+ </tile>
+ <tile id="80">
+  <image source="../../images/images_tiles/tile81.png" width="48" height="48"/>
+ </tile>
+ <tile id="81">
+  <image source="../../images/images_tiles/tile82.png" width="48" height="48"/>
+ </tile>
+ <tile id="82">
+  <image source="../../images/images_tiles/tile83.png" width="48" height="48"/>
+ </tile>
+ <tile id="83">
+  <image source="../../images/images_tiles/tile84.png" width="48" height="48"/>
+ </tile>
+ <tile id="84">
+  <image source="../../images/images_tiles/tile85.png" width="48" height="48"/>
+ </tile>
+ <tile id="85">
+  <image source="../../images/images_tiles/tile86.png" width="48" height="48"/>
+ </tile>
+ <tile id="86">
+  <image source="../../images/images_tiles/tile87.png" width="48" height="48"/>
+ </tile>
+ <tile id="87">
+  <image source="../../images/images_tiles/tile88.png" width="48" height="48"/>
+ </tile>
+ <tile id="88">
+  <image source="../../images/images_tiles/tile89.png" width="48" height="48"/>
+ </tile>
+ <tile id="89">
+  <image source="../../images/images_tiles/tile90.png" width="48" height="48"/>
+ </tile>
+ <tile id="90">
+  <image source="../../images/images_tiles/tile91.png" width="48" height="48"/>
+ </tile>
+ <tile id="91">
+  <image source="../../images/images_tiles/tile92.png" width="48" height="48"/>
+ </tile>
+ <tile id="92">
+  <image source="../../images/images_tiles/tile93.png" width="48" height="48"/>
+ </tile>
+ <tile id="93">
+  <image source="../../images/images_tiles/tile94.png" width="48" height="48"/>
+ </tile>
+ <tile id="94">
+  <image source="../../images/images_tiles/tile95.png" width="48" height="48"/>
+ </tile>
+ <tile id="95">
+  <image source="../../images/images_tiles/tile96.png" width="48" height="48"/>
+ </tile>
+ <tile id="96">
+  <image source="../../images/images_tiles/tile97.png" width="48" height="48"/>
+ </tile>
+ <tile id="97">
+  <image source="../../images/images_tiles/tile98.png" width="48" height="48"/>
+ </tile>
+ <tile id="98">
+  <image source="../../images/images_tiles/tile99.png" width="48" height="48"/>
+ </tile>
+ <tile id="99">
+  <image source="../../images/images_tiles/tile100.png" width="48" height="48"/>
+ </tile>
+ <tile id="100">
+  <image source="../../images/images_tiles/tile101.png" width="48" height="48"/>
+ </tile>
+ <tile id="101">
+  <image source="../../images/images_tiles/tile102.png" width="48" height="48"/>
+ </tile>
+ <tile id="102">
+  <image source="../../images/images_tiles/tile103.png" width="48" height="48"/>
+ </tile>
+ <tile id="103">
+  <image source="../../images/images_tiles/tile104.png" width="48" height="48"/>
+ </tile>
+ <tile id="104">
+  <image source="../../images/images_tiles/tile105.png" width="48" height="48"/>
+ </tile>
+ <tile id="105">
+  <image source="../../images/images_tiles/tile106.png" width="48" height="48"/>
+ </tile>
+ <tile id="106">
+  <image source="../../images/images_tiles/tile107.png" width="48" height="48"/>
+ </tile>
+ <tile id="107">
+  <image source="../../images/images_tiles/tile108.png" width="48" height="48"/>
+ </tile>
+ <tile id="108">
+  <image source="../../images/images_tiles/tile109.png" width="48" height="48"/>
+ </tile>
+ <tile id="109">
+  <image source="../../images/images_tiles/tile110.png" width="48" height="48"/>
+ </tile>
+ <tile id="110">
+  <image source="../../images/images_tiles/tile111.png" width="48" height="48"/>
+ </tile>
+ <tile id="111">
+  <image source="../../images/images_tiles/tile112.png" width="48" height="48"/>
+ </tile>
+ <tile id="112">
+  <image source="../../images/images_tiles/tile113.png" width="48" height="48"/>
+ </tile>
+ <tile id="113">
+  <image source="../../images/images_tiles/tile114.png" width="48" height="48"/>
+ </tile>
+ <tile id="114">
+  <image source="../../images/images_tiles/tile115.png" width="48" height="48"/>
+ </tile>
+ <tile id="115">
+  <image source="../../images/images_tiles/tile116.png" width="48" height="48"/>
+ </tile>
+ <tile id="116">
+  <image source="../../images/images_tiles/tile117.png" width="48" height="48"/>
+ </tile>
+ <tile id="117">
+  <image source="../../images/images_tiles/tile118.png" width="48" height="48"/>
+ </tile>
+ <tile id="118">
+  <image source="../../images/images_tiles/tile119.png" width="48" height="48"/>
+ </tile>
+ <tile id="119">
+  <image source="../../images/images_tiles/tile120.png" width="48" height="48"/>
+ </tile>
+ <tile id="120">
+  <image source="../../images/images_tiles/tile121.png" width="48" height="48"/>
+ </tile>
+ <tile id="121">
+  <image source="../../images/images_tiles/tile122.png" width="48" height="48"/>
+ </tile>
+ <tile id="122">
+  <image source="../../images/images_tiles/tile123.png" width="48" height="48"/>
+ </tile>
+ <tile id="123">
+  <image source="../../images/images_tiles/tile124.png" width="48" height="48"/>
+ </tile>
+ <tile id="124">
+  <image source="../../images/images_tiles/tile125.png" width="48" height="48"/>
+ </tile>
+ <tile id="125">
+  <image source="../../images/images_tiles/tile126.png" width="48" height="48"/>
+ </tile>
+ <tile id="126">
+  <image source="../../images/images_tiles/tile127.png" width="48" height="48"/>
+ </tile>
+ <tile id="127">
+  <image source="../../images/images_tiles/tile128.png" width="48" height="48"/>
+ </tile>
+</tileset>
